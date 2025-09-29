826a3cd2ecd0a4f9e863a7ae5c9af2009262f1d2
Dhanuka-12 <danukupe@gmail.com>
2025-09-30 01:13:04 +0530

    feat: set up Express and dotenv configuration in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-console.log("Hello, World!");
+import { Express } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PHONE_NUMBER_ID = process.env.PHONE_NUMBER_ID;
+const VERSION = process.env.VERSION;
+const WHATSAPP_USER_ACCESS_TOKEN = process.env.WHATSAPP_USER_ACCESS_TOKEN;
+
+const app = express();
+
