1318105d089dede066df162c0fa613bac0e91dde
Dhanuka-12 <danukupe@gmail.com>
2025-10-01 02:44:11 +0530

    feat: refactor message sending logic into MessageController and create app configuration

--- src/app.ts
+++ src/app.ts
@@ -1,58 +1,21 @@
 import express from "express";
-import dotenv from "dotenv";
+
 import axios from "axios";
+import { APP_CONFIG } from "./config/app.config";
+import { MessageController } from "./controller/message.controller";
 
-dotenv.config();
 
-const PHONE_NUMBER_ID = process.env.PHONE_NUMBER_ID;
-const VERSION = process.env.VERSION;
-const WHATSAPP_USER_ACCESS_TOKEN = process.env.WHATSAPP_USER_ACCESS_TOKEN;
+
+
 
 const app = express();
 
 app.use(express.json());
 
-app.post("/send-message", async (req, res) => {
-    const { phoneNumber, message } = req.body;
-    await sendMessage(phoneNumber, message);
-    res.status(200).json({message: "Message sent" });
+const messageController = new MessageController();
 
-});
+app.post("/send-message",messageController.sendMessage);
 
-
-async function sendMessage(phoneNumber: string, message: string) {
-
-    let data = JSON.stringify({
-        "messaging_product": "whatsapp",
-        "recipient_type": "individual",
-        "to": `${phoneNumber}`,
-        "type": "text",
-        "text": {
-            "preview_url": false,
-            "body": message
-        }
-    });
-
-    let config = {
-        method: 'post',
-        maxBodyLength: Infinity,
-        url: `https://graph.facebook.com/${VERSION}/${PHONE_NUMBER_ID}/messages`,
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${WHATSAPP_USER_ACCESS_TOKEN}`
-        },
-        data: data
-    };
-
-    await axios.request(config)
-        .then((response:any) => {
-            console.log(JSON.stringify(response.data));
-        })
-        .catch((error:any) => {
-            console.log(error);
-        });
-
-}
 
 app.listen(8558, () => {
     console.log("Server is running on port 8558");    
