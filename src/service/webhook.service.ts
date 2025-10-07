8110e5c6404221d465edff4913058ac7f8f78ea5
Dhanuka-12 <danukupe@gmail.com>
2025-10-08 00:21:22 +0530

    feat: implement singleton pattern in webhookService class

--- src/service/webhook.service.ts
+++ src/service/webhook.service.ts
@@ -1,3 +1,19 @@
+import { get } from "http";
+
 export class webhookService {
-    
+    private static instance: webhookService;
+
+    public static getInstance(): webhookService {
+        if (!webhookService.instance) {
+            webhookService.instance = new webhookService();
+        }
+
+        return webhookService.instance;
+
+    }
+
+    private constructor() {
+
+    }
+
 }
